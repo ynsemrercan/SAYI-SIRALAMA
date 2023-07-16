6b5d600eefd19463a25a5797f83f7cee86138cbf
ynsemrercan <137263703+ynsemrercan@users.noreply.github.com>
2023-07-16 11:16:55 +0300

    Add files via upload

---
+++ sayiSiralama.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class sayiSiralama {
+    public static void main(String[] args) {
+
+        Scanner input =new Scanner(System.in);
+
+        int a,b,c;
+
+        System.out.println("İlk sayıyı giriniz: ");
+        a=input.nextInt();
+
+        System.out.println("İkinci sayıyı giriniz: ");
+        b=input.nextInt();
+
+        System.out.println("Üçüncü sayıyı giriniz: ");
+        c=input.nextInt();
+
+
+        if ((a<b) && (a<c)){
+
+            if (b<c){
+
+                System.out.println("a<b<c");
+            }
+            else {
+                System.out.println("a<c<b");
+            }
+        }
+        else if(b<a && b<c) {
+
+            if (a<c){
+
+                System.out.println("b<a<c");
+            }
+            else{
+
+                System.out.println("b<c<a");
+            }
+
+
+        }
+        else {
+
+            if (a<b){
+
+                System.out.println("c<a<b");
+            }
+            else{
+
+                System.out.println("c<b<a");
+            }
+        }
+
+
+
+
+
+}}
